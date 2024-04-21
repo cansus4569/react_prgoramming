4f93996c7c7fc08febe87855e166b59b49f8ea68
cansus4569 <pjs25563@gmail.com>
2024-04-21 21:40:11 +0900

    - 재사용성을 고려한 컴포넌트 구분법 - App에 다 정의된 코드를 컴포넌트와 컨테이너로 나눔

---
+++ section5/재사용성을 고려한 컴포넌트 구분법/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FriendPage from './container/FriendPage';
+
+function App() {
+  return (
+    <div>
+      <FriendPage />
+    </div>
+  );
+}
+
+export default App;
